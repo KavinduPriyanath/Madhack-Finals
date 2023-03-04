daaee9082fd81d3cac2ea56e00d56668507afa63
Inuri Lavanya <lilh99lilh@gmail.com>
2023-03-04 20:35:44 +0530

    Implement the view of the event page

--- lib/Society/view_event.dart
+++ lib/Society/view_event.dart
@@ -42,11 +42,178 @@ class _ViewSocietyEventsState extends State<ViewSocietyEvents> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Events"),
+        title: Text('WORKSHOP 2'),
       ),
 
       //body
+      body: Padding(
 
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(width: 30.0),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+              child: Center(
+                child: Image(
+                 image: AssetImage('assets/society_event-1.jpeg'),
+                  height: 200,
+                  width: 200,
+                ),
+              ),
+            ),
+
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(1),
+                  child: Card(
+                    color: Colors.blue[200],
+                    child: Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [Text(
+                          'Date: ',
+                          style: TextStyle(
+                              letterSpacing: 2.0,
+                            fontSize:14.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                          Text(
+                            '04/03/2023',
+                            style: TextStyle(
+                              color: Colors.black,
+                              letterSpacing: 2.0,
+                            ),
+                          ),
+                        ],
+                      ),
+                  ),
+                  ),
+                ),
+              ],
+            ),
+
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(1),
+                    child: Card(
+                      color: Colors.blue[200],
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [Text(
+                            'Time: ',
+                            style: TextStyle(
+                                letterSpacing: 2.0,
+                              fontSize:14.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                            Text(
+                              '8.30 AM',
+                              style: TextStyle(
+                                color: Colors.black,
+                                letterSpacing: 2.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(1),
+                    child: Card(
+                      color: Colors.blue[200],
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [Text(
+                            'Venue: ',
+                            style: TextStyle(
+                                letterSpacing: 2.0,
+                              fontSize:14.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                            Text(
+                              'UCSC Premises',
+                              style: TextStyle(
+                                color: Colors.black,
+                                letterSpacing: 2.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(1),
+                    child: Card(
+                      color: Colors.blue[200],
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [Text(
+                            'Description: ',
+                            style: TextStyle(
+                                letterSpacing: 2.0,
+                              fontSize:14.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                            Text(
+                              'The 2nd workshop of the MADHACK will be held on 4th March 2023.',
+                              style: TextStyle(
+                                color: Colors.black,
+                                letterSpacing: 2.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+        ],
+        ),
+      ),
       //bottom navbar
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentPageIndex, // highlight the current icon
