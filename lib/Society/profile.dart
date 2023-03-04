b4ef41c2d5843c0f09d1e6d2b131bd7a1496d75b
Inuri Lavanya <lilh99lilh@gmail.com>
2023-03-04 21:47:02 +0530

    Add edit button and the navigations

--- lib/Society/profile.dart
+++ lib/Society/profile.dart
@@ -33,15 +33,88 @@ class _ProfileState extends State<Profile> {
       }
     });
   }
+
+  void editProfile() {
+    Navigator.pushNamed(context, "/editprofile");
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Profile"),
+        title: Text("IEEE Student Branch"),
       ),
 
       //body
+      body: Padding(
 
+        padding: EdgeInsets.fromLTRB(0, 40.0, 0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(width: 30.0),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 0, 0, 20),
+              child: Center(
+                child: Image(
+                  image: AssetImage('assets/IEEE-background.jpg'),
+                  height: 200,
+                  width: 200,
+                ),
+              ),
+            ),
+
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(0),
+                    child: Card(
+                      color: Colors.blue[200],
+                      child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [Text(
+                            'Description: ',
+                            style: TextStyle(
+                              letterSpacing: 2.0,
+                              fontSize:14.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                            Text(
+                              'The IEEE Student Branch of University of Colombo School of Computing was established in 2011 and has achieved great success during the past few years. The student branch has been formed with the participation and contribution of all four batches of University of Colombo School of Computing and it has organized many activities to enhance professional development of the students.',
+
+                              style: TextStyle(
+                                color: Colors.black,
+                                letterSpacing: 2.0,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 30,),
+
+            Center(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue[600],
+                ),
+                onPressed:editProfile,
+                child: Text('Edit Content'),
+              ),
+            ),
+          ],
+        ),
+      ),
 
       //bottom navbar
       bottomNavigationBar: BottomNavigationBar(
